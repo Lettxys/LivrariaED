fe7e0d7892ceab3f0f5a4a7edd8c518d597ebb6e
João Soares <jmkoygg@gmail.com>
2025-02-13 10:19:09 -0300

    Salvando user na main

--- Livraria/main.java
+++ Livraria/main.java
@@ -1,6 +1,12 @@
 package Livraria;
+
+import Livraria.SU.Usuario;
+import Livraria.SU.UsuarioServices;
+
 public class Main {
     public static void main(String args[]) {
-        LoginSystem.iniciar();
+        int id = LoginSystem.iniciar();
+        Usuario User = UsuarioServices.BuscarUsuarioPorId(id);
+        System.out.println("Bem vindo, " + User.getNome());
     }
 }
